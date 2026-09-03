f5c07c3b5a4220858940272683778b63a9ae1181
Srabonti <somayaaktarshabonti721@gmail.com>
2026-09-03 21:45:16 +0600

    added jobs collections and server side insert all jobs

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("hellow world")
-})
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efzq5bn.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,10 +18,20 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-client.connect().catch(console.dir)
+client.connect().catch(console.dir);
+const jobsCollections = client.db("JobPortal").collection("Jobs");
+
+app.get("/jobs", async(req, res)=>{
+  const cursor = jobsCollections.find();
+  const result = await cursor.toArray();
+  res.send(result)
+})
+
 //  client.db("admin").command({ ping: 1 });
 //     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
+app.get("/", (req, res)=>{
+    res.send("job portal all jobs ")
+})
 app.listen(port,()=>{
-    console.log(`Example app listening on port:${port}`);  
+    console.log(`job portal app listening on port:${port}`);  
 })
